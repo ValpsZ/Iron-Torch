b1ab7adfb1da54c82d1dcbd05d091e598c7c6513
Emil Lindgren <limelgren@gmail.com>
2023-06-26 19:25:20 +0200

    change version number, and fixed errors in iron_torch.rs

--- src/iron_torch.rs
+++ src/iron_torch.rs
@@ -4,36 +4,36 @@ mod activation;
 pub struct Layer<'a> {
     weights: matrix::Matrix,
     biases: matrix::Matrix,
-    data: matrix::Matrix,
+    data: Option<matrix::Matrix>,
     activation: activation::Activation<'a>
 }
 
 impl Layer<'_> {
-    fn new<'a>(size: usize, activation: activation::Activation<'a>) -> Layer {
+    pub fn new<'a>(input_size: usize, output_size: usize, activation: activation::Activation<'a>) -> Layer {
         Layer {
-            weights: matrix::Matrix::random(size, 1),
-            biases: matrix::Matrix::random(size, 1),
-            data: matrix::Matrix::zeros(size, 1),
+            weights: matrix::Matrix::random(input_size, output_size),
+            biases: matrix::Matrix::random(input_size, output_size),
+            data: None,
             activation
         }
     }
-    
-    fn forward(&mut self, input: Vec<f64>) -> Vec<f64> {
+
+    pub fn forward(&mut self, input: Vec<f64>) -> Vec<f64> {
         if input.len() != self.weights.cols {
             panic!("Input length ({}) does not match the number of columns in the weights matrix ({})", input.len(), self.weights.cols);
         }
-        
+
         let mut output = matrix::Matrix::from(vec![input]).transpose();
         output = self.weights
             .multiply(&output)
             .add(&self.biases)
             .map(self.activation.function);
-        self.data = output.clone();
-        
+        self.data = Some(output.clone());
+
         output.data[0].to_owned()
     }
 
-    fn backwards(&mut self, outputs: Vec<f64>, targets: Vec<f64>, learning_rate: f64) {
+    pub fn backwards(&mut self, outputs: Vec<f64>, targets: Vec<f64>, learning_rate: f64) {
         if targets.len() != outputs.len() {
             panic!("Length mismatch: targets length ({}) does not match outputs length ({})", targets.len(), outputs.len());
         }
@@ -42,7 +42,10 @@ impl Layer<'_> {
         let errors = matrix::Matrix::from(vec![targets]).subtract(&parsed);
         let mut gradients = parsed.map(self.activation.derivativ).dot_product(&errors).map(&|x| x * learning_rate);
 
-        self.weights = self.weights.add(&gradients.multiply(&self.data.transpose()));
+        match self.data {
+            Some(ref mut data) => self.weights = self.weights.add(&gradients.multiply(&data.transpose())),
+            None => panic!("Data is missing. Cannot perform the operation."),
+        }
         self.biases = self.biases.add(&gradients);
     }
 }
